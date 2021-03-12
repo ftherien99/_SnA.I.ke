5e3a7df6574f8eaf15cecacbadefea91a07c1883
MrRadish99 <69933825+MrRadish99@users.noreply.github.com>
2021-03-11 20:17:56 -0500

    Main Menu

---
+++ Code/Button.py
@@ -0,0 +1,27 @@
+import pygame
+pygame.init()
+
+class Button:
+    def __init__(self, height, width, posX, posY, color, text):
+        self.height = height
+        self.width = width
+        self.posX = posX
+        self.posY = posY
+        self.color = color
+        self.text = text
+        
+      
+    
+    def drawButton(self, window):
+        pygame.draw.rect(window, self.color, (self.posX, self.posY, self.width, self.height), 0)
+        textFont = pygame.font.SysFont("arial", 24)
+        buttonText = textFont.render(self.text, 1, (0,0,0))
+        window.blit(buttonText, (self.posX + (self.width/2 - buttonText.get_width()/2), self.posY + (self.height/2 - buttonText.get_height()/2)))
+
+    def clicked(self, mousePos):
+        if mousePos[0] > self.posX and mousePos[0] < self.posX + self.width:
+            if mousePos[1] > self.posY and mousePos[1] < self.posY + self.height:
+                return True
+                
+        
+        return False
---
+++ Code/Main.py
@@ -0,0 +1,136 @@
+import pygame
+from Button import Button
+
+class Main:
+    def __init__(self):
+        self.showMainMenu()
+        self.menuWindow = None
+        self.running = True
+        self.currentMenu = "MainMenu"
+        self.showMainMenu()
+        self.setGameConfig()
+        self.setSimulationConfig()
+        
+        #Window loop
+        while self.running:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            if self.currentMenu == "MainMenu":
+                self.showMainMenu()
+            elif self.currentMenu == "GameConfig":
+                self.setGameConfig()
+            elif self.currentMenu == "SimulationConfig":
+                self.setSimulationConfig()
+                    
+
+    def showMainMenu(self):
+
+        buttonX = 395
+        buttonY = 200
+            
+        (width, height) = (1000, 800)
+        self.menuWindow = pygame.display.set_mode((width,height))
+        pygame.display.set_caption("S N A. I. K E")
+
+        font = pygame.font.SysFont("arial", 60)
+        text = font.render("S  N  A.  I.  K  E", 1, (0,255,0))
+
+        self.menuWindow.blit(text, (335,75))
+        
+
+        gameButton = Button(75,225, buttonX, buttonY, (0,255,0), "Play")
+        gameButton.drawButton(self.menuWindow)
+
+        simulationButton = Button(75,225, buttonX, buttonY + 150, (0,255,0), "Simulation")
+        simulationButton.drawButton(self.menuWindow)
+
+        graphButton = Button(75,225, buttonX, buttonY + 300, (0,255,0), "Diagrams")
+        graphButton.drawButton(self.menuWindow)
+
+        quitButton = Button(75,225, buttonX, buttonY + 450, (0,255,0), "Quit")
+        quitButton.drawButton(self.menuWindow)
+
+        pygame.display.update()
+        
+        for event in pygame.event.get():
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+
+                mousePos = pygame.mouse.get_pos()
+
+                if gameButton.clicked(mousePos):
+                    self.currentMenu = "GameConfig"
+                elif simulationButton.clicked(mousePos):
+                    self.currentMenu = "SimulationConfig"
+                elif graphButton.clicked(mousePos):
+                    pass
+                elif quitButton.clicked(mousePos):
+                    self.running = False
+
+            elif event.type == pygame.QUIT:
+                    self.running = False
+
+    
+    def setGameConfig(self):
+
+        self.menuWindow.fill((0,0,0))
+        
+        startButton = Button(75,225, 250, 675, (0,255,0), "Start Game")
+        startButton.drawButton(self.menuWindow)
+
+        cancelButton = Button(75,225, 550, 675, (255,0,0), "Cancel")
+        cancelButton.drawButton(self.menuWindow)
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mousePos = pygame.mouse.get_pos()
+
+                if startButton.clicked(mousePos):
+                    pass
+                elif cancelButton.clicked(mousePos):
+                    self.currentMenu = "MainMenu"
+
+            elif event.type == pygame.QUIT:
+                    self.running = False
+
+        pygame.display.update()
+
+    
+    def setSimulationConfig(self):
+
+        self.menuWindow.fill((0,0,0))
+        
+
+        startButton = Button(75,225, 250, 675, (0,255,0), "Start Game")
+        startButton.drawButton(self.menuWindow)
+
+        cancelButton = Button(75,225, 550, 675, (255,0,0), "Cancel")
+        cancelButton.drawButton(self.menuWindow)
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mousePos = pygame.mouse.get_pos()
+
+                if startButton.clicked(mousePos):
+                    pass
+                elif cancelButton.clicked(mousePos):
+                    self.currentMenu = "MainMenu"
+
+            elif event.type == pygame.QUIT:
+                    self.running = False
+
+        pygame.display.update()
+
+        
+
+
+    
+
+
+if __name__ == "__main__":
+    m = Main() 
