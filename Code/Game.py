5fbb54c063b0d42991f5e43ed4d8e9266a4aff63
MrRadish99 <69933825+MrRadish99@users.noreply.github.com>
2021-03-16 14:59:26 -0400

    All windows added

---
+++ Code/Game.py
@@ -0,0 +1,59 @@
+import pygame
+from Button import Button
+from SnakeDAO import SnakeDAO
+
+
+class Game:
+    def __init__(self, main): #headColor, bodyColor, appleColor, speed, boardSize
+        self.snakeDAO = SnakeDAO()
+        self.main = main
+        self.gameWindow = self.main.menuWindow
+        self.showGame()
+        
+
+
+    def showGame(self):
+        buttonX = 750
+        buttonY = 900
+
+        self.gameWindow.fill((0,0,0))
+
+        pygame.display.set_caption("S N A. I. K E")
+
+        gameBoard = pygame.draw.rect(self.gameWindow, (0,255,0), (100, 50, 1300, 800), 2)
+
+        font = pygame.font.SysFont("arial", 28)
+        scoreText = font.render("Score:", 1, (0,255,0))
+        self.gameWindow.blit(scoreText, (100,925))
+
+        score = font.render("0", 1, (0,255,0))
+        self.gameWindow.blit(score, (180,925))
+
+        highscoreText = font.render("Highscore:", 1, (0,255,0)) #va aller chercher la valeur dans la bd
+        self.gameWindow.blit(highscoreText, (375,925))
+
+
+        pauseButton = Button(75,225, buttonX, buttonY, (0,255,0), "Pause")
+        pauseButton.drawButton(self.gameWindow)
+
+        resetButton = Button(75,225, buttonX + 250, buttonY, (0,255,0), "Reset")
+        resetButton.drawButton(self.gameWindow)
+
+        quitButton = Button(75,225, buttonX + 500, buttonY, (255,0,0), "Quit")
+        quitButton.drawButton(self.gameWindow)
+
+        
+
+        if pygame.mouse.get_pressed() == (1,0,0):
+            mousePos = pygame.mouse.get_pos()
+
+            if pauseButton.clicked(mousePos):
+                pass
+            elif resetButton.clicked(mousePos):
+                pass
+            elif quitButton.clicked(mousePos):
+                self.main.currentMenu = "MainMenu"
+
+           
+
+        pygame.display.update()
--- Code/Main.py
+++ Code/Main.py
@@ -1,6 +1,13 @@
 import pygame
 from Button import Button
 from SnakeDAO import SnakeDAO
+from Game import Game
+from Simulation import Simulation
+from Graph import Graph
+
+##GROS BOARD = 1300x800
+##PETIT BOARD = 1000x700
+##PAS DE PETIT BOARD POUR AI, JUSTE LE GROS /3
 
 class Main:
     def __init__(self):
@@ -13,6 +20,9 @@ class Main:
         self.setSimulationConfig()
         self.snakeDAO = SnakeDAO()
         self.snakeDAO.dbConnection()
+        self.currentGame = None
+        self.currentSimulation = None
+        self.graphWindow = None
         
         #Window loop
         while self.running:
@@ -28,21 +38,27 @@ class Main:
                 self.setGameConfig()
             elif self.currentMenu == "SimulationConfig":
                 self.setSimulationConfig()
+            elif self.currentMenu == "Game":
+                self.currentGame.showGame()
+            elif self.currentMenu == "Simulation":
+                self.currentSimulation.showSimulation()
+            elif self.currentMenu == "Graph":
+                self.graphWindow.showGraphWindow()
                     
 
     def showMainMenu(self):
 
-        buttonX = 395
-        buttonY = 200
+        buttonX = 650
+        buttonY = 250
             
-        (width, height) = (1000, 800)
+        (width, height) = (1500, 1000)
         self.menuWindow = pygame.display.set_mode((width,height))
         pygame.display.set_caption("S N A. I. K E")
 
         font = pygame.font.SysFont("arial", 60)
         text = font.render("S  N  A.  I.  K  E", 1, (0,255,0))
 
-        self.menuWindow.blit(text, (335,75))
+        self.menuWindow.blit(text, (585,75))
         
 
         gameButton = Button(75,225, buttonX, buttonY, (0,255,0), "Play")
@@ -57,51 +73,47 @@ class Main:
         quitButton = Button(75,225, buttonX, buttonY + 450, (0,255,0), "Quit")
         quitButton.drawButton(self.menuWindow)
 
+        if pygame.mouse.get_pressed() == (1,0,0):
+            mousePos = pygame.mouse.get_pos()
+
+            if gameButton.clicked(mousePos):
+                self.currentMenu = "GameConfig"
+            elif simulationButton.clicked(mousePos):
+                self.currentMenu = "SimulationConfig"
+            elif graphButton.clicked(mousePos):
+                self.graphWindow = Graph(self)
+                self.currentMenu = "Graph"
+            elif quitButton.clicked(mousePos):
+                self.snakeDAO.dbCloseConnection()
+                self.running = False
+
         pygame.display.update()
         
-        for event in pygame.event.get():
-
-            if event.type == pygame.MOUSEBUTTONDOWN:
-
-                mousePos = pygame.mouse.get_pos()
-
-                if gameButton.clicked(mousePos):
-                    self.currentMenu = "GameConfig"
-                elif simulationButton.clicked(mousePos):
-                    self.currentMenu = "SimulationConfig"
-                elif graphButton.clicked(mousePos):
-                    pass
-                elif quitButton.clicked(mousePos):
-                    self.snakeDAO.dbCloseConnection()
-                    self.running = False
-
-            elif event.type == pygame.QUIT:
-                    self.snakeDAO.dbCloseConnection()
-                    self.running = False
 
     
     def setGameConfig(self):
 
         self.menuWindow.fill((0,0,0))
+
+        font = pygame.font.SysFont("arial", 60)
+        text = font.render("Configuration", 1, (0,255,0))
+
+        self.menuWindow.blit(text, (625,75))
         
-        startButton = Button(75,225, 250, 675, (0,255,0), "Start Game")
+        startButton = Button(75,225, 500, 825, (0,255,0), "Start Game")
         startButton.drawButton(self.menuWindow)
 
-        cancelButton = Button(75,225, 550, 675, (255,0,0), "Cancel")
+        cancelButton = Button(75,225, 800, 825, (255,0,0), "Cancel")
         cancelButton.drawButton(self.menuWindow)
 
-        for event in pygame.event.get():
+        if pygame.mouse.get_pressed() == (1,0,0):
+            mousePos = pygame.mouse.get_pos()
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mousePos = pygame.mouse.get_pos()
-
-                if startButton.clicked(mousePos):
-                    pass
-                elif cancelButton.clicked(mousePos):
-                    self.currentMenu = "MainMenu"
-
-            elif event.type == pygame.QUIT:
-                    self.running = False
+            if startButton.clicked(mousePos):
+                self.currentGame = Game(self)
+                self.currentMenu = "Game"
+            elif cancelButton.clicked(mousePos):
+                self.currentMenu = "MainMenu"
 
         pygame.display.update()
 
@@ -109,26 +121,27 @@ class Main:
     def setSimulationConfig(self):
 
         self.menuWindow.fill((0,0,0))
-        
 
-        startButton = Button(75,225, 250, 675, (0,255,0), "Start Game")
+        font = pygame.font.SysFont("arial", 60)
+        text = font.render("Configuration", 1, (0,255,0))
+
+        self.menuWindow.blit(text, (625,75))
+        
+        startButton = Button(75,225, 500, 825, (0,255,0), "Start Game")
         startButton.drawButton(self.menuWindow)
 
-        cancelButton = Button(75,225, 550, 675, (255,0,0), "Cancel")
+        cancelButton = Button(75,225, 800, 825, (255,0,0), "Cancel")
         cancelButton.drawButton(self.menuWindow)
 
-        for event in pygame.event.get():
+        if pygame.mouse.get_pressed() == (1,0,0):
+            mousePos = pygame.mouse.get_pos()
+         
+            if startButton.clicked(mousePos):
+                self.currentSimulation = Simulation(self)
+                self.currentMenu = "Simulation"
+            elif cancelButton.clicked(mousePos):
+                self.currentMenu = "MainMenu"
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mousePos = pygame.mouse.get_pos()
-
-                if startButton.clicked(mousePos):
-                    pass
-                elif cancelButton.clicked(mousePos):
-                    self.currentMenu = "MainMenu"
-
-            elif event.type == pygame.QUIT:
-                    self.running = False
 
         pygame.display.update()
 
