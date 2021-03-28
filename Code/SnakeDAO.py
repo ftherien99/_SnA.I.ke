569f2c78ea7dd658cec5bc6777d9b0b4c1f5a687
MrRadish99 <69933825+MrRadish99@users.noreply.github.com>
2021-03-28 16:36:48 -0400

    Save and get game colors

--- Code/SnakeDAO.py
+++ Code/SnakeDAO.py
@@ -15,15 +15,14 @@ class SnakeDAO:
             )
 
             self.cursor = self.connection.cursor()
-            
+
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
         finally:
-            if self.cursor is not None:
+            if self.connection is not None:
                 print("Database connection established")
 
-    def dbCloseConnection(self):
-        self.cursor.close()     
+    def dbCloseConnection(self):     
         self.connection.close()
         print("Database connection closed")
 
@@ -48,13 +47,51 @@ class SnakeDAO:
                     return row[4]
            
         except:
-            print("Error while fetching data")
+            print("Error while fetching highscore data")
+
 
     def saveHighscore(self,gameType, highscore):
         query = "UPDATE highscores SET " + gameType + " = %s WHERE id = 1"
         try:
-            self.cursor.execute(query,[highscore])
+            self.cursor.execute(query,(highscore))
 
             self.connection.commit()
         except:
-            print("Faield to save data")
+            print("Failed to save highscore data")
+
+
+
+    def getColors(self, gameObject):
+        try:
+            query = "SELECT * FROM colors"
+            self.cursor.execute(query)
+            records = self.cursor.fetchall()
+
+            #transformation int to RGB: https://stackoverflow.com/questions/2262100/rgb-int-to-rgb-python
+            for row in records:
+                if gameObject == "head":
+                    color = (row[1] // 256 // 256 % 256, row[1] // 256 % 256, row[1] % 256)
+                    return color
+                elif gameObject == "body":
+                    color = (row[2] // 256 // 256 % 256, row[2] // 256 % 256, row[2] % 256)
+                    return color
+                elif gameObject == "apple":
+                    color = (row[3] // 256 // 256 % 256, row[3] // 256 % 256, row[3] % 256)
+                    return color
+           
+        except:
+            print("Error while fetching color data")
+
+
+
+    def saveColors(self,headColor, bodyColor, appleColor):
+        headColorInt = (headColor[0] * 256 * 256) + (headColor[1] * 256) + headColor[2]
+        bodyColorInt = (bodyColor[0] * 256 * 256) + (bodyColor[1] * 256) + bodyColor[2]
+        appleColorInt = (appleColor[0] * 256 * 256) + (appleColor[1] * 256) + appleColor[2]
+
+        query = "UPDATE colors SET head_color = %s, body_color = %s, apple_color = %s WHERE id = 1"
+        try:
+            self.cursor.execute(query,(headColorInt, bodyColorInt, appleColorInt))
+            self.connection.commit()
+        except:
+            print("Failed to save color data")
